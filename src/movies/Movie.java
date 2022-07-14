7f7e8ed27c5c0ad1fb77055cd3a227c714d93024
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-07-14 13:28:04 -0500

    started movies package

---
+++ src/movies/Movie.java
@@ -0,0 +1,11 @@
+package movies;
+
+public class Movie {
+    String name;
+    String category;
+
+    Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
+}
